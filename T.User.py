be25ece0b6bec5ee76966d27fa5948e4634acabe
michael <mikelkarije@gmail.com>
2019-08-30 18:54:38 +0300

    this commit will add a user array,name ,phone number and email

---
+++ T.User.py
@@ -0,0 +1,12 @@
+class User:
+    """
+    a class that generates new instances of Users.
+    """
+    pass
+
+    Users_arry = []
+    def __init__(self, first_name, last_name, phone_number, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.email = email
