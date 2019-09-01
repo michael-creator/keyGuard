b7909dd4b15d79a437c285880f413dd874078a91
michael <mikelkarije@gmail.com>
2019-09-01 18:18:30 +0300

    add new changes

--- T.User.py
+++ T.User.py
@@ -1,11 +1,13 @@
-class User:
+class Person:
+
     """
     class that generates new instances of Users.
     """
     pass
 
-    Users_arry = []
+    person_arry = []
     def __init__(self, first_name, last_name, phone_number, email):
+        
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = phone_number
---
+++ T.User_test.py
@@ -0,0 +1,11 @@
+import unittest # Importing the unittest module
+from person import Person # Importing the User class
+
+class TestUser(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the User class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
