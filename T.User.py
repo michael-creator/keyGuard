2cad95ccfbf57372d566b30b1b1eeece473d0011
michael <mikelkarije@gmail.com>
2019-09-02 08:16:42 +0300

    this commit add new list

--- T.User.py
+++ T.User.py
@@ -5,10 +5,17 @@ class Person:
     """
     pass
 
-    person_arry = []
+
+    person_list = []
     def __init__(self, first_name, last_name, phone_number, email):
         
-        self.first_name = first_name
-        self.last_name = last_name
-        self.phone_number = phone_number
-        self.email = email
+        self.first_name = first_name,"mickey"
+        self.last_name = last_name,"karije"
+        self.phone_number = phone_number,"0712345678"
+        self.email = "mikelkarije@gmail.com"
+        
+    def save_person(self):
+        '''
+        Method that saves a person to person list
+        '''
+        Person.person_list.append(self)
--- T.User_test.py
+++ T.User_test.py
@@ -6,8 +6,6 @@ class TestPerson(unittest.TestCase):
     '''
     Test class that defines test cases for the User class behaviours.
 
-    Args:
-        unittest.TestCase: TestCase class that helps in creating test cases
     '''
     def setUp(self):
         '''
