d164fe1b2ea2502034a4b7616f4061123b73a0f0
michael <mikelkarije@gmail.com>
2019-09-02 10:24:28 +0300

    commit adds pyperclip

---
+++ credential.py
@@ -0,0 +1,12 @@
+import string
+import pyperclip
+from random import choice
+
+class credential:
+    Credential_list = []
+
+    def __init__(self,account_name, account_number,email)
+
+    self.account_name = accountk
+    self.account_number =
+    self.email =
