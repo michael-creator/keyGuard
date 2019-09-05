810b94895055ebcce4c97d51e6cd21f7fab11557
michael <mikelkarije@gmail.com>
2019-09-05 15:47:44 +0300

    addition of login details for the user after creating an account

--- run.py
+++ run.py
@@ -26,18 +26,34 @@ def del_user(user):
   
 def main():
 
-  print("Hello welcome to password locker")
-  print("lets begin to create an account")
-  print("_"*25)
-  print("i. create Account")
-  print("Enter username")
-  user = input()#details of your username
-  print("Enter password")
-  user.password = input()
-  save_users(create_user(user,password)
-  print(f{user}welcome you have created an account with us. please enjoy )
+    print("Hello welcome to password locker")
+    print("lets begin to create an account")
+    print("_"*25)
+    print("i. create Account")
+    print("Enter username")
+    user = input()#details of your username
+    print("Enter password")
+    password = input()
+    save_users(create_user(user,password)
+    print(f"Welcome {user} you have created an account with us, please enjoy")
 
+    while True:
+        print("use this short code:cc-to create an account, dc-to display user_accounts, fc -to find user credentials") 
+        short_code = input().lower()
 
+        if short_code == 'cc':
+        print("New Account")
+        print("-"*25)
+        print ("user_name ....")
+        User_name = input()
+
+        print("password ..."
+        password = input()
+        save_users(create_user("User_name,password")) # create and save new user Account.
+
+        elif short_code == 'dc':
+        if display_users():
+                            
 
   if __name__ == "__main__":
     main()
